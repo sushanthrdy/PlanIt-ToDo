53ed644b760e54e85850278aba208e2017a4a879
Sushanth Reddy Daram <sushanth.reddy@hidubai.com>
2024-10-07 21:56:16 +0400

    Initial commit

---
+++ app/src/main/java/com/sushanth/pitd/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.sushanth.pitd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sushanth.pitd.ui.theme.PlanItToDoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlanItToDoApp()
+        }
+    }
+}
+
+@Composable
+fun PlanItToDoApp(modifier: Modifier = Modifier) {
+    PlanItToDoTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PlanItToDoTheme {
+        Greeting("Android")
+    }
+}
